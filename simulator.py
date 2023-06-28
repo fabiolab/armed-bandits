638737893242f3704968c9d1142ff884e4710a73
Fabrice Depaulis <fabrice.depaulis@orange.com>
2023-06-28 08:56:04 +0200

    refactor: move non business code to utils.py file

--- simulator.py
+++ simulator.py
@@ -1,14 +1,9 @@
-from typing import Dict, List, Optional
-
-import numpy as np
-import pandas as pd
-import plotly.graph_objects as go
-
-from bandit_random import RandomBandits
-from bandit_successive_elimination import SuccessiveEliminationBandits
-from bandit_ucb import UCBBandits
-
+from src.bandit_random import RandomBandits
+from src.bandit_successive_elimination import SuccessiveEliminationBandits
+from src.bandit_ucb import UCBBandits
+from src.utils import display_data, update_viz_data
 from numpy.random import binomial
+
 
 NB_ACTIONS = 5
 NB_ROUNDS = 50000
@@ -17,68 +12,8 @@ BEST_ACTION_DELTA = 0.2
 WINDOW_SIZE = 500
 
 bandits = RandomBandits(NB_ACTIONS)
-bandits = UCBBandits(NB_ACTIONS)
-bandits = SuccessiveEliminationBandits(NB_ACTIONS)
-
-
-# Generate a list of sublists of a given window size from a source list and compute the average value
-def compute_moving_average(data: List[float], window_size: int) -> List[float]:
-    return list(np.convolve(data, np.ones(window_size), "valid") / window_size)
-
-
-def display_data(data: Dict, title: str):
-    data["moving_mean_regret"] = compute_moving_average(
-        data["instant_regret"], WINDOW_SIZE
-    )
-    data["moving_mean_regret"] += data["moving_mean_regret"][-WINDOW_SIZE + 1 :]
-
-    df = pd.DataFrame(data=data)
-    fig = go.Figure()
-
-    fig.add_trace(go.Scatter(x=df.rounds, y=df["0"], name="Action 0 (Best)"))
-    for i in range(1, NB_ACTIONS):
-        fig.add_trace(go.Scatter(x=df.rounds, y=df[str(i)], name=f"Action {i}"))
-    fig.add_trace(go.Scatter(x=df.rounds, y=df["mean_regret"], name="Regret"))
-    fig.add_trace(go.Scatter(x=df.rounds, y=df["mean_reward"], name="Mean Reward"))
-    fig.add_trace(
-        go.Scatter(x=df.rounds, y=df["ab_mean_reward"], name="AB Mean Reward")
-    )
-    fig.add_trace(
-        go.Scatter(x=df.rounds, y=df["moving_mean_regret"], name="Moving Mean Regret")
-    )
-    fig.update_traces(mode="lines")
-    fig.update_layout(
-        title=title, xaxis=dict(title="Rounds"), yaxis=dict(title="Reward")
-    )
-    fig.show()
-
-
-def update_viz_data(
-    data: Optional[Dict[str, List[float]]], round_number: int
-) -> Dict[str, List[float]]:
-
-    if not data:
-        data: Dict[str, List[float]] = {str(i): [] for i in range(NB_ACTIONS)}
-        data["rounds"] = []
-        data["mean_regret"] = []
-        data["instant_regret"] = []
-        data["mean_reward"] = []
-        data["ab_mean_reward"] = []
-
-    data["rounds"].append(round_number)
-    for action in bandits.actions:
-        data[str(action.action_id)].append(action.mean_reward)
-    regret = (
-        bandits.actions[BEST_ACTION_ID].mean_reward * (round_number + 1)
-        - bandits.total_cumulative_reward
-    )
-    data["mean_regret"].append(regret / (round_number + 1))
-    data["instant_regret"].append(bandits.actions[BEST_ACTION_ID].mean_reward - reward)
-    data["mean_reward"].append(bandits.total_cumulative_reward / (round_number + 1))
-    data["ab_mean_reward"].append(
-        (0.5 + BEST_ACTION_DELTA + (0.5 * (NB_ACTIONS - 1))) / NB_ACTIONS
-    )
-    return data
+# bandits = UCBBandits(NB_ACTIONS)
+# bandits = SuccessiveEliminationBandits(NB_ACTIONS)
 
 
 def play(action_id: int) -> int:
@@ -95,6 +30,18 @@ for current_round in range(NB_ROUNDS):
     reward: int = play(action_id)
     bandits.observe(action_id, reward)
 
-    visualization_data = update_viz_data(visualization_data, current_round)
+    visualization_data = update_viz_data(
+        bandits=bandits,
+        reward=reward,
+        data=visualization_data,
+        round_number=current_round,
+        best_action_id=BEST_ACTION_ID,
+        best_action_delta=BEST_ACTION_DELTA,
+    )
 
-display_data(visualization_data, str(bandits))
+display_data(
+    data=visualization_data,
+    title=str(bandits),
+    window_size=WINDOW_SIZE,
+    nb_actions=NB_ACTIONS,
+)
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,74 @@
-# -------------------------------  Copyright ---------------------------------
-# This software has been developed by Orange TGI/DATA-IA/AITT/aRod
-# Copyright (c) Orange TGI Data/IA 2022
-#
-# COPYRIGHT: This file is the property of Orange TGI.
-# Copyright Orange TGI 2022, All Rights Reserved
-#
-# This software is the confidential and proprietary information of Orange TGI.
-#
-# You shall not disclose such Confidential Information and shall use it only
-# in accordance with the terms of the license agreement you entered into with
-# Orange TGI.
-#
-# AUTHORS      : Orange TGI  TGI/DATA-IA/AITT/aRod
-# EMAIL        : data-ia.aitt-dev-rennes@orange.com
-# ----------------------------------------------------------------------------
+from typing import Dict, List, Optional
+
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+
+from src.bandit_system import BanditSystem
+
+
+# Generate a list of sublists of a given window size from a source list and compute the average value
+def compute_moving_average(data: List[float], window_size: int) -> List[float]:
+    return list(np.convolve(data, np.ones(window_size), "valid") / window_size)
+
+
+def display_data(data: Dict, title: str, window_size: int, nb_actions: int) -> None:
+    data["moving_mean_regret"] = compute_moving_average(
+        data["instant_regret"], window_size
+    )
+    data["moving_mean_regret"] += data["moving_mean_regret"][-window_size + 1 :]
+
+    df = pd.DataFrame(data=data)
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=df.rounds, y=df["0"], name="Action 0 (Best)"))
+    for i in range(1, nb_actions):
+        fig.add_trace(go.Scatter(x=df.rounds, y=df[str(i)], name=f"Action {i}"))
+    fig.add_trace(go.Scatter(x=df.rounds, y=df["mean_regret"], name="Regret"))
+    fig.add_trace(go.Scatter(x=df.rounds, y=df["mean_reward"], name="Mean Reward"))
+    fig.add_trace(
+        go.Scatter(x=df.rounds, y=df["ab_mean_reward"], name="AB Mean Reward")
+    )
+    fig.add_trace(
+        go.Scatter(x=df.rounds, y=df["moving_mean_regret"], name="Moving Mean Regret")
+    )
+    fig.update_traces(mode="lines")
+    fig.update_layout(
+        title=title, xaxis=dict(title="Rounds"), yaxis=dict(title="Reward")
+    )
+    fig.show()
+
+
+def update_viz_data(
+    bandits: BanditSystem,
+    reward: int,
+    data: Optional[Dict[str, List[float]]],
+    round_number: int,
+    best_action_id: int,
+    best_action_delta: float,
+) -> Dict[str, List[float]]:
+
+    nb_actions = len(bandits.actions)
+
+    if not data:
+        data: Dict[str, List[float]] = {str(i): [] for i in range(nb_actions)}
+        data["rounds"] = []
+        data["mean_regret"] = []
+        data["instant_regret"] = []
+        data["mean_reward"] = []
+        data["ab_mean_reward"] = []
+
+    data["rounds"].append(round_number)
+    for action in bandits.actions:
+        data[str(action.action_id)].append(action.mean_reward)
+    regret = (
+        bandits.actions[best_action_id].mean_reward * (round_number + 1)
+        - bandits.total_cumulative_reward
+    )
+    data["mean_regret"].append(regret / (round_number + 1))
+    data["instant_regret"].append(bandits.actions[best_action_id].mean_reward - reward)
+    data["mean_reward"].append(bandits.total_cumulative_reward / (round_number + 1))
+    data["ab_mean_reward"].append(
+        (0.5 + best_action_delta + (0.5 * (nb_actions - 1))) / nb_actions
+    )
+    return data
